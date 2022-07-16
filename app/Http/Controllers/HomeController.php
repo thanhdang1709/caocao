2ed771adfac7730ab6c8e80b63fbffc847114730
thanhcan <thanhcan1105@gmail.com>
2022-07-16 12:39:25 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,9 +36,29 @@ class HomeController extends Controller
             $query->where('email', $user_search)->orWhere('address', $user_search);
         }
 
-        $users = $query->paginate(20);
+        $users = $query->where('is_ban', 0)->paginate(20);
 
         return view('user.list', compact('users'));
+    }
+
+    public function banUser(Request $request){
+        $id = $request->id;
+        $status = User::where('id', $id)->update(['is_ban' => 1]);
+        return $this->responseOK([]);
+    }
+
+    public function getBannedUser(Request $request){
+        // $users = User::paginate(20);
+
+        $query = User::query();
+        $user_search = $request->user_search;
+        if ($user_search) {
+            $query->where('email', $user_search)->orWhere('address', $user_search);
+        }
+
+        $users = $query->where('is_ban', 1)->paginate(20);
+
+        return view('user.list_user_banned', compact('users'));
     }
 
 }
